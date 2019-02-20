d798485f7bbe760b81ce0f2ad1583eb7d0ee42fc
saud9030 <saud.amakn@gmail.com>
2019-02-20 16:35:44 +0300

    Updates

---
+++ precache-manifest.2255799e59bcb7fab4de6f33039151c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc72cdd150e98797c325",
+    "url": "/ToDoList/static/css/main.ab796518.chunk.css"
+  },
+  {
+    "revision": "cc72cdd150e98797c325",
+    "url": "/ToDoList/static/js/main.cc72cdd1.chunk.js"
+  },
+  {
+    "revision": "ca9d33dca053b8c7bab6",
+    "url": "/ToDoList/static/js/runtime~main.ca9d33dc.js"
+  },
+  {
+    "revision": "8fa3261057a4606c044d",
+    "url": "/ToDoList/static/js/2.8fa32610.chunk.js"
+  },
+  {
+    "revision": "9547ed0cab8bda6d6f692aa3b1c3e277",
+    "url": "/ToDoList/static/media/twitter.9547ed0c.svg"
+  },
+  {
+    "revision": "c7978d435230536aade1113647c04e6f",
+    "url": "/ToDoList/static/media/github.c7978d43.svg"
+  },
+  {
+    "revision": "4de961fd2bdefd4a8810b1223cd4d95b",
+    "url": "/ToDoList/static/media/linkedin.4de961fd.svg"
+  },
+  {
+    "revision": "2965cc9a2ef278aba592bcb70e16ea6c",
+    "url": "/ToDoList/index.html"
+  }
+];
